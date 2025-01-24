6716a7c1d60ea484f0400e606e1d38400a07e04e
jbrajkovic <brajkovic.josip96@gmail.com>
2025-01-25 00:30:29 +0100

    adressbar solution

--- src/App.vue
+++ src/App.vue
@@ -1,155 +1,89 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import { onMounted, onBeforeUnmount, ref } from "vue";
 
-function scrollToBottom() {
-  window.scrollTo({
-    behavior: 'smooth',
-    top: document.body.scrollHeight
-  })
-}
+export default {
+  setup() {
+    const fixedDiv = ref(null);
+
+    let ticking = false; // Flag za debouncing preko requestAnimationFrame
+
+    const syncWindowScroll = (event) => {
+      if (!ticking) {
+        ticking = true;
+        requestAnimationFrame(() => {
+          window.scrollTo({
+            top: event.target.scrollTop, // Sinkroniziraj window scroll
+          });
+          ticking = false;
+        });
+      }
+    };
+
+    onMounted(() => {
+      if (fixedDiv.value) {
+        fixedDiv.value.addEventListener("scroll", syncWindowScroll, {
+          passive: true, // Poboljšava performanse
+        });
+      }
+    });
+
+    onBeforeUnmount(() => {
+      if (fixedDiv.value) {
+        fixedDiv.value.removeEventListener("scroll", syncWindowScroll);
+      }
+    });
+
+    return {
+      fixedDiv,
+    };
+  },
+};
 </script>
+
+
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    TEST
   </header>
 
-  <main>
-    <button @click="scrollToBottom">SCROLL TO BOTTOM</button>
-    <TheWelcome />
-    <div class="scroll-trigger"></div>
-    <div class="fixed-div">
+  <main ref="mainElement">
+    <div class="fixed-div" ref="fixedDiv">
       <div class="fixed-div-title">IFRAME</div>
-      <div class="fixe-div-content">
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
-        <div>TEST</div>
+      <div class="fixed-div-content">
+        <div v-for="i in 100" :key="i">TEST {{ i }}</div>
       </div>
+    </div>
+    <div class="main-content">
+      <div v-for="i in 100" :key="'main' + i">Main Content {{ i }}</div>
     </div>
   </main>
 </template>
 
 <style scoped>
 header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-.scroll-trigger {
-  height: 1000px;
   width: 100%;
+  height: 100px;
+  background-color: orange;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+main {
+  height: calc(100dvh + 200px);
+  overflow-y: auto; /* Osiguraj da main ima skrol */
 }
 
 .fixed-div {
   position: fixed;
-  top: 0;
+  top: 100px;
   left: 0;
   background-color: aliceblue;
   width: 100%;
   height: 100dvh;
   color: #000;
+  overflow-y: auto; /* Skrol za fixed div */
+}
+
+.main-content {
+  padding-top: 100px;
 }
 </style>
